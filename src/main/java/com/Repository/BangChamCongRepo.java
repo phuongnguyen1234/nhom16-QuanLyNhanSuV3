b33a16d1f602d92e10d1d3e22006e6cd4e74b9bf
kienkjt <143080078+kienkjt@users.noreply.github.com>
2024-11-15 18:12:59 +0700

    Update BangChamCongRepo.java

--- src/main/java/com/Repository/BangChamCongRepo.java
+++ src/main/java/com/Repository/BangChamCongRepo.java
@@ -1,8 +1,13 @@
 package com.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.Model.*;
+import com.Model.BangChamCong;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BangChamCongRepo extends JpaRepository<BangChamCong, String> {
-    
+    @Query("SELECT b FROM BangChamCong b WHERE MONTH(b.thoiGian) = :month AND YEAR(b.thoiGian) = :year")
+    List<BangChamCong> findByMonthAndYear(@Param("month") int month, @Param("year") int year);
 }
