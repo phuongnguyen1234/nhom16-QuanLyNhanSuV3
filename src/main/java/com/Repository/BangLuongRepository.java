8b9a7e577b65defba9dd9b6536862ffdc03925a2
ThuHuong <doanthithuhuong240104@gmail.com>
2024-11-16 16:44:00 +0700

    Update BangLuongRepository.java

--- src/main/java/com/Repository/BangLuongRepository.java
+++ src/main/java/com/Repository/BangLuongRepository.java
@@ -5,12 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-@Repository
 public interface BangLuongRepository extends JpaRepository<BangLuong, String> {
-
-    @Query("SELECT bl FROM BangLuong bl " +
-           "JOIN bl.bangChamCong bcc " +
-           "WHERE MONTH(bcc.thoiGian) = :month AND YEAR(bcc.thoiGian) = :year")
-    List<BangLuong> findByMonthAndYear(@Param("month") int month, @Param("year") int year);
+    List<BangLuong> findByBangChamCong_ThoiGian_MonthAndBangChamCong_ThoiGian_Year(int month, int year);
 }
+
 
