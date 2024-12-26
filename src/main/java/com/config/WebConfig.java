160e2311bbaac5821e381a97b5e48280c330c9dc
Kien04840 <163751438+Kien04840@users.noreply.github.com>
2024-12-26 13:58:41 +0700

    Update WebConfig.java

--- src/main/java/com/config/WebConfig.java
+++ src/main/java/com/config/WebConfig.java
@@ -1,14 +1,20 @@
 package com.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private SessionInterceptor sessionInterceptor;
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -21,8 +27,22 @@ public class WebConfig implements WebMvcConfigurer {
         // Cho phép tất cả các nguồn truy cập vào API
         registry.addMapping("/api/**")
                 .allowedOrigins("http://127.0.0.1:5500") // Cấu hình nguồn frontend của bạn
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Các phương thức HTTP cho phép
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Các phương thức HTTP cho phép
                 .allowedHeaders("*"); // Cho phép tất cả các header
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(sessionInterceptor)
+                .addPathPatterns("/**") // Áp dụng cho tất cả các URL
+                .excludePathPatterns("/pages-login.html", "/assets/**"); // Loại trừ URL không cần kiểm tra
+    }
+
+     @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCachePeriod(0) // Không cache
+                .setCacheControl(org.springframework.http.CacheControl.noStore()); // Ngăn cache
+    }
 }
