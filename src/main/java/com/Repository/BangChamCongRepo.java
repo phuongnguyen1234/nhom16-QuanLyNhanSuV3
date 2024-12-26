4bb93691bb6f2fcfce7095a4d15686971722e191
Nguyễn Trung Kiên <143080078+kienkjt@users.noreply.github.com>
2024-12-26 10:42:22 +0700

    Update BangChamCongRepo.java

--- src/main/java/com/Repository/BangChamCongRepo.java
+++ src/main/java/com/Repository/BangChamCongRepo.java
@@ -1,11 +1,15 @@
+
 package com.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.Model.*;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.time.*;
 import java.util.*;
 
 public interface BangChamCongRepo extends JpaRepository<BangChamCong, String> {
-    List<BangChamCong> findByThoiGian(LocalDate thoiGian);
-    boolean existsByMaNhanSu(String maNhanSu);
+    @Query("SELECT b FROM BangChamCong b WHERE MONTH(b.thoiGian) = :month AND YEAR(b.thoiGian) = :year")
+    List<BangChamCong> findByMonthAndYear(@Param("month") int month, @Param("year") int year);
 }
